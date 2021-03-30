b8dfa2517905fadbf136a3e819ad75d545c6bb6f
CarstenHennig <carsten.hennig@gmail.com>
2021-03-30 10:17:05 +0200

    done

---
+++ carsten-language-statements.js
@@ -0,0 +1,10 @@
+let message = "Hello World";
+console.log(message);
+
+// Without "" error "Unexpected identifier" appears
+
+/* 
+const newMessage = "This is my first JS file."
+console.log(newMessage);
+ */
+
